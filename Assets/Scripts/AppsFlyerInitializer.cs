b0b3968203fad452bfd58b953da22e5438329fbf
Mehmet Musa <mmusa@sabanciuniv.edu>
2019-08-15 15:52:24 +0300

    15.08.2019v2

--- Assets/Scripts/AppsFlyerInitializer.cs
+++ Assets/Scripts/AppsFlyerInitializer.cs
@@ -7,19 +7,19 @@ public class AppsFlyerInitializer : MonoBehaviour
     void Start()
     {
         /* Mandatory - set your AppsFlyer’s Developer key. */
-        AppsFlyer.setAppsFlyerKey("YOUR_APPSFLYER_DEV_KEY");
+        AppsFlyer.setAppsFlyerKey("zcKrZYJWnrWWctCxcLNnyT");
         /* For detailed logging */
         /* AppsFlyer.setIsDebug (true); */
 #if UNITY_IOS
   /* Mandatory - set your apple app ID
    NOTE: You should enter the number only and not the "ID" prefix */
-  AppsFlyer.setAppID ("YOUR_APP_ID_HERE");
+  AppsFlyer.setAppID ("1474653825");
   AppsFlyer.trackAppLaunch ();
 #elif UNITY_ANDROID
         /* Mandatory - set your Android package name */
-        AppsFlyer.setAppID("YOUR_ANDROID_PACKAGE_NAME_HERE");
+        AppsFlyer.setAppID("com.UtmostGames.BoomBalls");
         /* For getting the conversion data in Android, you need to add the "AppsFlyerTrackerCallbacks" listener.*/
-        AppsFlyer.init("YOUR_APPSFLYER_DEV_KEY", "AppsFlyerTrackerCallbacks");
+        AppsFlyer.init("zcKrZYJWnrWWctCxcLNnyT", "AppsFlyerTrackerCallbacks");
 #endif
     }
 
